799d158bf8b34e2fbd1c62f18165ca2ed8a64924
annaludmila13 <annaludmila10@gmail.com>
2024-12-17 20:02:44 +0300

    Add files via upload

---
+++ mobule_1_2.py
@@ -0,0 +1,6 @@
+total_homeworks = 12
+spent_hours = 1.5
+course_title = 'Python'
+average_time_per_task = spent_hours / total_homeworks
+
+print(f'Курс: {course_title}, всего задач: {total_homeworks}, затрачено часов: {spent_hours}, среднее время выполнения {average_time_per_task:.3f} часа.')
